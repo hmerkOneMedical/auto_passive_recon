2a1d1fc89d247890b1f0792c1edfe2c021e98e4e
hmerkOneMedical <hmerk@m-6615.omg.lan>
2019-07-15 15:14:05 -0700

    refactoring

--- main.py
+++ main.py
@@ -8,69 +8,18 @@ import googleScraper
 import json
 import shodan
 
-SHODAN_API_KEY = os.environ['SHODAN_API_KEY']
-
-
-Gr = '\033[90m'   # grey
-G = '\033[92m'  # green
-Y = '\033[93m'  # yellow
-B = '\033[94m'  # blue
-R = '\033[91m'  # red
-W = '\033[0m'   # white
-BOLD = '\033[1m'
-END = '\033[0m'
-
-def formatInput(promter):
-    return raw_input(BOLD + promter+ END + ' \n>>  ')
-
-def pprint(log, indent, unwanted_keys):
-    tabs = indent*'\t' or ''
-    if (isinstance(log, str) or (isinstance(log, int))):
-        print(tabs+str(log))
-    elif (isinstance(log,list)):
-        try:
-            print(tabs + ', '.join(log))
-        except:
-                for item in log:
-                    if (isinstance(item, str) or (isinstance(item, int))):
-                        print(tabs + str(item))
-                    else:
-                        pprint(item, indent+1, unwanted_keys)
-                    print('\n')
-    elif (isinstance(log, dict)):
-        if unwanted_keys:
-            for unwanted_key in unwanted_keys: 
-                if unwanted_key in log:
-                    del log[unwanted_key]
-
-        for key in log.keys():
-            if isinstance(log[key], str) or isinstance(log[key], int):
-                print(tabs+key+': '+str(log[key]))
-            else:
-                print(tabs+''+key+':')
-                pprint(log[key], indent+1, unwanted_keys)
-    elif (isinstance(log, unicode)):
-        pprint(log.encode('utf-8'), indent, unwanted_keys)
-    else:
-        print(str(tabs)+str(log))
-
-def formatResponse(title, log = '', unwanted_keys = []):
-    print("%s==========================%s" % (Gr, W))
-    print("%s%s%s%s%s" % (B, BOLD, title, W, END))
-    print(pprint(log, 0, unwanted_keys))
-    
+from helpers import *
 #==================
 
+SHODAN_API_KEY = os.environ['SHODAN_API_KEY']
+
+#==================
 companyUrl = formatInput('URL')
 companyName = formatInput('NAME')
 subdomainOutFile = formatInput('FILE PATH FOR SUBDOMAINS (use .txt extension)')
 
-try:
-    basics = (scrapeCrunchbase.companyBasicInfo(companyName))
-    formatResponse('Basic Company Information (Crunchbase)', basics, [])
-except:
-    formatResponse('no crunchbase record for company named '+ companyName, '', [])
-
+basics = (scrapeCrunchbase.companyBasicInfo(companyName))
+formatResponse('Basic Company Information (Crunchbase)', basics, [])
 
 domain = whois.query(companyUrl)
 scrape_employees_query = googleScraper.query('site:www.linkedin.com/in security cyber ' + companyName, 10)
--- scrapeCrunchbase.py
+++ scrapeCrunchbase.py
@@ -50,7 +50,10 @@ def companyBasicInfo(name):
     url = 'https://www.crunchbase.com/v4/data/entities/organizations/' + name + \
     '?field_ids=%5B%22identifier%22,%22layout_id%22,%22facet_ids%22,%22title%22,%22short_description%22,%22is_unlocked%22%5D&layout_mode=view'
 
-    response = requests.request("GET", url, headers=headers)    
-    res =  yaml.safe_load(response.text)['cards']
+    try:
+        response = requests.request("GET", url, headers=headers)    
+        res =  yaml.safe_load(response.text)['cards']
 
-    return formatResponse(res)
+        return formatResponse(res)
+    except e:
+        return 'Scraping crunchbase failed.'
