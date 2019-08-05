ca601ff93e602e6cd20d95ded8ee346f9a1b3f06
Helena Merk <hmerk@onemedical.com>
2019-08-05 14:42:20 -0700

    testing async

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 __author__ = 'helena merk'
 
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, jsonify, url_for
+from celery import Celery
 
 import json
 from scripts.recon import run_recon
@@ -10,6 +11,9 @@ import requests
 from selenium import webdriver
 import base64
 import time
+import random
+import redis
+
 
 from scripts import *
 from scripts.helpers import *
@@ -17,6 +21,17 @@ from scripts.render_helpers import *
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+redis_url = redis.from_url(os.environ.get("REDIS_URL"))
+
+
+app.config['CELERY_BROKER_URL'] = redis_url
+app.config['CELERY_RESULT_BACKEND'] = redis_url
+
+
+# Initialize Celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 printable = set(string.printable)
 
@@ -126,6 +141,87 @@ def report():
         yield report_footer_html()
     
     return Response(generate(), mimetype='text/html')
+
+@celery.task(bind=True)
+def long_task(self):
+    """Background task that runs a long function with progress reports."""
+    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0} {1} {2}...'.format(random.choice(verb),
+                                              random.choice(adjective),
+                                              random.choice(noun))
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
+
+
+@app.route('/long_task_demo', methods=['GET', 'POST'])
+def long_task_demo():
+    if request.method == 'GET':
+        return render_template('long_task.html', )
+
+    # # send the email
+    # email_data = {
+    #     'subject': 'Hello from Flask',
+    #     'to': email,
+    #     'body': 'This is a test email sent from a background Celery task.'
+    # }
+    # if request.form['submit'] == 'Send':
+    #     # send right away
+    #     send_async_email.delay(email_data)
+    #     flash('Sending email to {0}'.format(email))
+    # else:
+    #     # send in one minute
+    #     send_async_email.apply_async(args=[email_data], countdown=60)
+    #     flash('An email will be sent to {0} in one minute'.format(email))
+
+    return redirect(url_for('long_task_demo'))
+
+
+@app.route('/longtask', methods=['POST'])
+def longtask():
+    task = long_task.apply_async()
+    return jsonify({}), 202, {'Location': url_for('taskstatus',
+                                                  task_id=task.id)}
+
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
+
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=3003)
