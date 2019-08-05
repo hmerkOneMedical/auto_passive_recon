925d8309ab2369f9298abf4c32af9eec16698ad5
Helena Merk <hmerk@onemedical.com>
2019-08-05 09:59:59 -0700

    dynamic rendering

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-__author__ = 'helena'
+__author__ = 'helena merk'
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, session
+
 import json
 from scripts.recon import run_recon
 import string
@@ -8,9 +9,14 @@ import os
 import requests
 from selenium import webdriver
 import base64
-import uuid
+import time
+
+from scripts import *
+from scripts.helpers import *
+from scripts.render_helpers import *
 
 app = Flask(__name__, static_url_path='/static')
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 printable = set(string.printable)
 
@@ -24,6 +30,7 @@ def frame():
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument("--test-type")
+    options.add_argument("--headless")
     driver = webdriver.Chrome(chrome_options=options)
     driver.get(url)
 
@@ -35,10 +42,14 @@ def frame():
 
     return '<div style="max-width: 100%; height: auto"><img style="max-width: 100%; height: auto" src="'+decodedScreenie+'"/></div>'
 
+## Statically renders report template
 @app.route('/recon', methods=['POST'])
 def recon():
     company_url = request.form['company_url']
     company_name = (request.form['company_name']).lower()
+
+    session['company_url'] = company_url
+    session['company_name'] = company_name
 
     response = run_recon(company_url, company_name, None, False)
     details = response['details']
@@ -54,35 +65,75 @@ def recon():
     for job in employees:
         job['description'] = filter(lambda x: x in printable, job['description'])
 
-    print domain_results
-
     current_ip = os.getenv('NGROK_EXPOSED_IP', '0.0.0.0')
 
     return render_template('report_details.html', EXPOSED_IP=current_ip, whois_result=whois_result, company_name=company_name, details=details, jobs=jobs, employees=employees, domain_results=domain_results, founder_emails=founder_emails)
 
+## Renders dynamically. Why? Heroku limits requests to 30 seconds, and /recon route times out.
+@app.route('/report', methods=['POST'])
+def report():
+    current_ip = os.getenv('NGROK_EXPOSED_IP', '127.0.0.1:5000')
 
-# @app.route('/printing_test')
-# def printing_test():
-#     return render_template('printing_test.html')
+    company_url = request.form['company_url']
+    company_name = (request.form['company_name']).lower()
 
-# @app.route('/tests')
-# def tests():
+    session['company_url'] = company_url
+    session['company_name'] = company_name
+
+    def format_json_save(key, value):
+        session[key] = json.dumps(value)
+
+    def callback_for_saving(data):
+        for key, val in data.iteritems():
+            format_json_save(key, value)
+
+    def generate():
+        #session['test'] = 'yes'
+        yield report_header_html(company_name)
+
+        details = scrape_crunchbase.run_basics(company_name)
+        
+        yield details_html(details) 
+
+        yield report_summary_html()
     
-#     jobs = [{'description': '4 days ago - Provides advice and guidance on security strategies to manage identified ... Maintains current knowledge of malware attacks, and other cyber\xa0...', 'link': 'https://www.linkedin.com/jobs/view/senior-manager-information-security-at-adidas-1335984743?trk=guest_job_details_job_title', 'title': 'adidas hiring Senior Manager Information Security in Portland, OR, US ...'}, {'description': 'Jul 9, 2019 - The Cyber Security Risk Assessor identifies cyber security risks to HP assets utilizing defined\u2026See this and similar jobs on LinkedIn.', 'link': 'https://www.linkedin.com/jobs/view/cyber-security-risk-assessor-governance-risk-and-compliance-at-hp-1363229618', 'title': 'HP hiring Cyber Security Risk Assessor - Governance Risk and ...'}, {'description': 'adidasPortland, OR, US. 3 months ago Be among ... ', 'link': 'https://www.linkedin.com/jobs/view/director-information-security-csirt-at-adidas-1300484323', 'title': 'adidas hiring Director Information Security CSIRT in Portland, OR, US ...'}, {'description': 'Protect the enterprise against potential cyber-attacks. Play a key role in all CSIRT (Cyber Security Incident Response Team) activities, responding to potential\xa0...', 'link': 'https://www.linkedin.com/jobs/view/manager-cyber-security-incident-response-forensics-m-f-d-at-adidas-1108039796?trkInfo=searchKeywordString%3AComputer%2BForensics%2CsearchLocationString%3A%252C%2B%2Cvertical%3Ajobs%2CpageNum%3A0%2Cposition%3A7%2CMSRPsearchId%3Ab338c090-d2cf-48ec-bb06-1199acd1c1d6&refId=b338c090-d2cf-48ec-bb06-1199acd1c1d6&trk=jobs_jserp_job_listing_text', 'title': 'adidas hiring Manager Cyber Security Incident Response & Forensics ...'}, {'description': 'Senior IT Consultant, Application Security. adidasPortland, OR, US. 8 months ago Be in the first 30 applicants. No longer accepting applications\xa0...', 'link': 'https://www.linkedin.com/jobs/view/senior-it-consultant-application-security-at-adidas-932600104', 'title': 'adidas hiring Senior IT Consultant, Application Security in Portland ...'}, {'description': 'adidasHerzogenaurach, DE. 5 months ago Be in the first 30 .... Consultant (m/w/d) f\xfcr Cyber Security und Informationssicherheit. univativ GmbH. Stuttgart, DE.', 'link': 'https://www.linkedin.com/jobs/view/senior-manager-information-security-governance-m-f-d-at-adidas-1042474708?trkInfo=searchKeywordString%3AAdidas%2BGroup%2BSenior%2CsearchLocationString%3A%252C%2B%2Cvertical%3Ajobs%2CpageNum%3A1%2Cposition%3A10%2CMSRPsearchId%3A15c032c4-0c56-4a7a-acd3-b7ee66e4762c&refId=15c032c4-0c56-4a7a-acd3-b7ee66e4762c&trk=jobs_jserp_job_listing_text', 'title': 'adidas hiring Senior Manager Information Security Governance (m/f/d ...'}, {'description': 'Operations Engineer - Network Security. adidasPortland, OR, US. 3 months ago Be among the first 25 applicants. No longer accepting applications\xa0...', 'link': 'https://www.linkedin.com/jobs/view/operations-engineer-network-security-at-adidas-1278439970', 'title': 'adidas hiring Operations Engineer - Network Security in Portland, OR ...'}, {'description': 'adidasHerzogenaurach, DE ... Responsibility for Security Governance Management< Proactively identify security gaps and support .... Solutions Engineer (Cyber Security Services) - Amsterdam, Netherlands - (Remote) - Secureworks Sales.', 'link': 'https://www.linkedin.com/jobs/view/1242624856?trk=d_flagship3_salary_explorer&refId=ff31e0e9-c8ce-4e56-8f81-e744cab2dd9c', 'title': 'adidas hiring Senior Manager Information Security ... - LinkedIn'}, {'description': "The Senior Information Security Architect is responsible for ensuring the completeness (fitness-for-purpose) and integrity of adidas' information security architecture, ... business requirements with information and cyber security requirements.", 'link': 'https://www.linkedin.com/jobs/view/senior-information-security-architect-at-adidas-group-1294186653?trk=jobs_job_title', 'title': '\u963f\u8fea\u8fbe\u65af hiring Senior Information Security Architect in Shanghai ...'}, {'description': 'Jul 8, 2019 - Group Manager, Information Security-&amp;amp;gt;&amp;amp;gt; Manages a ... and tacklingcomplex cyber security problems from strategy to execution. .... Senior Manager Information Security. adidas. Portland, OR, US.', 'link': 'https://www.linkedin.com/jobs/view/group-manager-information-security-threat-detection-at-efinancialcareers-1346274849', 'title': 'eFinancialCareers hiring Group Manager Information Security - Threat ...'}] 
-#     for job in jobs:
-#         job['description'] = filter(lambda x: x in printable, job['description'])
+        founder_emails = []
+        if details:
+            for founder in details['founders']:
+                founder_emails.append(hunter.get_work_email(company_url, founder))
 
-#     domain_results = [{'url': 'https://www.wikipedia.com', 'ip':'0.0.0.0', 'vulns':'123-Abs'}]
-#     return render_template('tests.html', data=jobs, domain_results=domain_results)
+        yield non_automated_checks(company_name, founder_emails)
 
+        # yield 'Step 3: Scraping the web...<br>' #str(founder_emails)
 
-# @app.route('/linkPreviews', methods=['GET'])
-# def linkPreviews():
-#     key = os.environ['LINKPREVIEW_API_KEY'];
-#     return (requests.get('http://api.linkpreview.net/?key='+key+'&q=https://www.onemedical.com')).json()
+        scrape_employees_query = google_scraper.query('site:www.linkedin.com/in \'' + company_name + '\' security cyber', 10)
 
+        scrape_jobs_query = google_scraper.query(
+        'site:www.linkedin.com/jobs \'' + company_name + '\' security cyber', 10)
 
+        yield linkedin_details_html(scrape_employees_query, scrape_jobs_query)
 
+        subdomains = sublist3r.main(
+                company_url, None, ports=None, silent=True, verbose=True, engines=None)
+
+        
+        
+        domain_results = query_shodan.add_domain_details(subdomains) #str(domain_results)
+        yield domain_html(domain_results)
+        # # format_json_save('domain_results', domain_results)
+
+        # data = {
+        #     'domain_results': domain_results, 
+        #     'employees': scrape_employees_query,
+        #     'jobs': scrape_jobs_query,
+        #     'details': details,
+        #     'founder_emails': founder_emails,
+        #     'whois_result': whois_result['data'],
+        # }
+
+        yield report_footer_html()
+    
+    return Response(generate(), mimetype='text/html')
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=3003)
---
+++ scripts/render_helpers.py
@@ -0,0 +1,294 @@
+def report_header_html(company_name):
+    return '''
+    <!DOCTYPE html>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link
+            href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"
+            rel="stylesheet"
+            id="bootstrap-css"
+            />
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
+        <link
+            href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css"
+            rel="stylesheet">
+        <script
+            src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+        <!------ Include the above in your HEAD tag ---------->
+        <html>
+            <head>
+                <title>Automated Report</title>
+                <link rel="stylesheet" type="text/css" href="../static/styles.css" />
+                <style type="text/css" media="print">
+                .dontprint
+                { display: none; }
+                </style>
+            </head>
+            <body>
+                <hr>
+                <div style="width:30px; height: 30px; display: inline-block;
+                    vertical-align:top;">
+                    <img
+                        src="https://www.onemedical.com/static/images/apple-touch-icon-180x180.png"
+                        style="max-width:100%;
+                        max-height:100%;">
+                </div>
+                <h1 style="display: inline-block; vertical-align:top;">Passive Recon
+                    Report: '''+ company_name +'''</h1>
+
+                <div class="dontprint">
+                    <hr>
+                    <h4><a target="_blank" href="/">Start Over</a></h4>
+                    <h4>To submit report, open the print module, and save as a PDF.<br>
+                        Email Security@ and we will get back to you shortly! <br>
+                        Note -- please be patient while the full report loads. It may take up to 2 minutes.
+                    </h4>
+                </div>
+                <hr>'''
+
+def report_footer_html():
+    return '''</body></html>'''
+
+def details_html(details):
+    founder_names = ', '.join(details['founders'])
+    online_info = ''
+    for det in details['online']:
+        online_info += '''<a target="_blank" href="'''+ details['online'][det] +'''">'''+ details['online'][det] +'''</a>'''
+        online_info += '<br>'
+
+    return '''
+        <div class="card" style="width: 49%; display: inline-block;">
+            <div class="card-body">
+                <h3 class="card-title">Company Details</h3>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Founded Date:
+                    ''' + details['founded_on'] + '''</li>
+                    <li class="list-group-item">Founders:
+                        ''' + founder_names + '''
+                    </li>
+                    <li class="list-group-item">Private/Public:
+                        ''' + details['company'] + '''</li>
+                    <li class="list-group-item">Number of Employees:
+                        ''' + details['num_employees'] + '''</li>
+                    <li class="list-group-item">Headquarters:
+                        ''' + details['location'] + '''</li>
+                    <li class="list-group-item">Online Presence: <br>
+                        ''' + online_info + '''
+                    </li>
+                </ul>
+            </div>
+        </div>
+        '''
+
+def report_summary_html():
+    return '''<!-- Report details -->
+        <div class="card" style="width: 49%; display: inline-block;
+            vertical-align:top;">
+            <div class="card-body">
+                <h3 class="card-title">Report Summary</h3>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item" id="toggle_breach_text"
+                        style="display:none;">This company has had security
+                        breaches in the past. We estimate the breach was
+                        significant as a public release was made.</li>
+                    <li class="list-group-item" id="toggle_pwned_text"
+                        style="display:none;">The founder's email was associated
+                        with other security breaches. This can indicate
+                        targetting, poor password rotation, or weak passwords.</li>
+                    <li class="list-group-item" id="toggle_staff_text"
+                        style="display:none;">There appears to be a lack of
+                        security engineers. This can present a significant
+                        security risk and can pose challenges in uncovering
+                        security breaches or timely remediating exploitable
+                        flaws in their environment.</li>
+                    <li class="list-group-item" id="toggle_vuln_text"
+                        style="display:none;">Our automated testing discovered
+                        vulnerabilites. See below for details.</li>
+                    <li class="list-group-item" id="toggle_high_amt_text"
+                        style="display:none;">The vast number of
+                        (sub)domains increases security risk.</li>
+                    <li class="list-group-item" id="toggle_vuln_text"
+                        style="display:block;">Because of the time boxed nature
+                        of the reconnaissance and because only publicly
+                        available data sources were used, this report is not
+                        meant to be indicative of overall security maturity of
+                        the company evaluated. Rather, it is meant to be used
+                        solely as a potential indicator that can be considered
+                        as one part of the overall diligence process.</li>
+                </ul>
+            </div>
+        </div> <hr>'''
+
+def non_automated_checks(company_name, founder_emails):
+    return '''<h3 style="page-break-before: always">Non-Automated Checks:</h3>
+        <div>
+            <h5 style="display: inline-block; width: 50%; vertical-align:top;">Have
+                founder emails been compromised? Visit <a target="_blank"
+                    href="https://haveibeenpwned.com">HaveIBeenPwned</a> to
+                check''' + " ,".join(founder_emails) + ''' </h5>
+            <div style="display: inline-block; vertical-align:top; right: 40%;">
+                <input
+                    type="checkbox"
+                    data-toggle="toggle"
+                    data-on="Yes"
+                    data-off="No"
+                    id="toggle_pwned"
+                    />
+            </div>
+            <h5 style="display: inline-block; width: 50%; vertical-align:top;">Have
+                there been previous security breaches?</h5>
+            <div style="display: inline-block; vertical-align:top; right: 40%;">
+                <input
+                    type="checkbox"
+                    data-toggle="toggle"
+                    data-on="Yes"
+                    data-off="No"
+                    id="toggle_breach"
+                    />
+            </div>
+            <br>
+            <div class="dontprint">
+                <iframe style="outline:none;width: 500px;height: 500px;"
+                    src="https://www.google.com/search?igu=1&ei=&q=''' + company_name + ''' breach"
+                    style=></iframe>
+            </div>
+        </div>
+        <hr>
+
+        <script> 
+      $(function() {
+        $('#toggle_breach').change(function() {
+          $('#toggle_breach_text').toggle();
+        })
+      })
+      $(function() {
+        $('#toggle_pwned').change(function() {
+          $('#toggle_pwned_text').toggle();
+        })
+      })
+      </script>'''
+
+def format_single_table_row(domain, index):
+    EXPOSED_IP = 'http://127.0.0.1:5000'
+    tool_str = ''
+    print(domain)
+    for tool_type in domain['builtwith']:
+        tool_str += tool_type +': '+ ' ,'.join(domain['builtwith'][tool_type]) + '<br>'
+
+    vuln_str = ''
+    for vuln in domain['vulns']:
+        vuln_str += '''<a target="_blank"
+                            href="https://nvd.nist.gov/vuln/detail/''' + str(vuln) + '''">
+                            ''' + str(vuln) + '''
+                        </a><br>'''
+
+    ip_str = '<br>'.join(domain['ips']) if domain['ips'] else ''
+    port_str = ''
+    for port in domain['ports']:
+        port_str += str(port) + '<br>'
+
+    growing_str = '''<tr data-toggle="collapse" data-target="#object-'''+str(index)+'''" class="accordion-toggle">
+                    <td><a target="_blank" href="https://'''+domain['url']+'''">
+                            '''+domain['url']+'''
+                        </a></td>
+                    <td>''' + ip_str + '''</td>
+                    <td>''' + port_str + '''</td>
+                    <td>''' + tool_str + '''</td>
+                    <td class="text-error">''' + vuln_str + '''</td>
+                </tr>
+                <tr>
+                <td colspan="6" class="hiddenRow"><div class="accordian-body
+                            collapse" id="object-''' + str(index)+ '''"><iframe
+                                src="'''+ str(EXPOSED_IP) +'''/frame?url=http://'''+ str(domain['url']) +'''" width="100%" height="200px"
+                                style="resize: both;"></iframe>
+                        </div> </td>
+                    
+                </tr>'''
+
+    return growing_str
+
+def domain_html(subdomains):
+    tbody_str = ''
+    for i in range(len(subdomains)):
+        tbody_str += format_single_table_row(subdomains[i], i)
+
+    return '''
+            <table class="table table-condensed" style="border-collapse:collapse;">
+                <thead>
+                    <tr>
+                        <th>Subdomain</th>
+                        <th>IP Address</th>
+                        <th>Ports</th>
+                        <th>Built With</th>
+                        <th>Vulnerabilities</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ''' + tbody_str + '''
+                </tbody>
+            </table>
+            <hr>'''
+
+def linkedin_details_html(employees, jobs):
+    jobs_blob = ''
+    for result in jobs:
+        jobs_blob += '<tr>'
+        jobs_blob += '<td>'+result['title']+'</td>'
+        jobs_blob += '<td><a target="_blank" href={{'+result['link']+'}}>LinkedIn</a></td>'
+        jobs_blob += '<td>'+result['description']+'</td>'
+        jobs_blob += '</tr>'
+
+    employees_blob = ''
+    for result in employees:
+        employees_blob += '<tr>'
+        employees_blob += '<td>'+result['title']+'</td>'
+        employees_blob += '<td><a target="_blank" href={{'+result['link']+'}}>LinkedIn</a></td>'
+        employees_blob += '<td>'+result['description']+'</td>'
+        employees_blob += '</tr>'
+
+    return '''
+        <h3>LinkedIn Details</h3>
+        <h5 style="display: inline-block; width: 50%;">Information available
+            online regarding open positions and current employees, specifically
+            with "Cyber" or "Security" in their title. These may all be false
+            positives. Do the results indicate a concerning/lack of engineers?</h5>
+        <div style="display: inline-block; vertical-align:top; right: 40%;">
+            <input
+                type="checkbox"
+                data-toggle="toggle"
+                data-on="Yes"
+                data-off="No"
+                id="toggle_staff"
+                />
+        </div>
+        <br>
+        <script>
+        $(function() {
+            $('#toggle_staff').change(function() {
+            $('#toggle_staff_text').toggle();
+            })
+        })
+        </script>
+        <a name="targetname"></a>
+        <div class="dontprint">
+            <div style="width: 49%; display: inline-block; vertical-align:top;">
+                <h3>Hiring</h3>
+                <table class="table">
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col" style="width: 100px;">LinkedIn</th>
+                        <th scope="col">Description</th>
+                    </tr>'''+ jobs_blob +'''
+                </table>
+            </div>
+            <div style="width: 49%; display: inline-block; vertical-align:top;">
+                <h3>Current Employees</h3>
+                <table class="table">
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col" style="width: 100px;">LinkedIn</th>
+                        <th scope="col">Description</th>
+                    </tr> ''' + employees_blob + '''
+                </table>
+            </div>
+        </div>'''
